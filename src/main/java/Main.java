32d4e9b7c9cd08b5e556c37d2ee8f8435aeda113
BloodySaint07 <66734010+BloodySaint07@users.noreply.github.com>
2024-02-21 21:07:30 +0530

    Update Main.java
    Developers Making Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Master");
         System.out.println("Hello Master Confirmed");
+
+        System.out.println("Hello Master Developer 1");
+        System.out.println("Hello Master Developer 2");
     }
 }
